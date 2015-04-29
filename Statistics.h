93601692baf5eebc02ec52e15e94403e1756c25a
robertleec <libinbin@libinbins-Mac-mini.local>
2015-04-29 14:29:21 +0800

    Refeacor vector element with template.

--- Statistics.h
+++ Statistics.h
@@ -4,17 +4,130 @@
 #include <vector>
 #include <map>
 #include <set>
+#include <cmath>
+#include "assert.h"
 
 namespace Math {
     
     using namespace std;
     
-    double sum(const vector<double>& values);
-    double mean(const vector<double>& values);
-    double sumOfSquares(const vector<double>& values);
-    double variance(const vector<double>& values);
-    double sampleStandardDeviation(const vector<double>& values);
-    size_t maxValueIndex(const vector<double>& values);
+    template<typename T>
+    double sum(const vector<T>& values) {
+        
+        assert(values.size() > 0);
+        
+        double sumValue = 0;
+        
+        typename vector<T>::const_iterator vectorIterator;
+        
+        for (vectorIterator = values.begin(); vectorIterator != values.end(); ++vectorIterator) {
+            sumValue += *vectorIterator;
+        }
+        
+        return sumValue;
+    }
+    
+    template<typename T>
+    double mean(const vector<T>& values) {
+        
+        assert(values.size() > 0);
+        
+        double result = 0;
+        
+        result = sum(values) / values.size();
+        
+        return result;
+    }
+    
+    template<typename T>
+    double sumOfSquares(const vector<T>& values) {
+        
+        assert(values.size() > 0);
+        
+        double result = 0;
+        
+        typename vector<T>::const_iterator vectorIterator;
+        
+        for (vectorIterator = values.begin(); vectorIterator != values.end(); ++vectorIterator) {
+            result += pow(*vectorIterator, 2);
+        }
+        
+        return result;
+    }
+    
+    template<typename T>
+    double variance(const vector<T>& values) {
+        
+        assert(values.size() > 0);
+        
+        double sumOfSquaresValue = sumOfSquares(values);
+        double meanValue = mean(values);
+        
+        double result;
+        
+        result = sumOfSquaresValue - pow(meanValue, 2);
+        
+        return result;
+    }
+    
+    template<typename T>
+    double sampleStandardDeviation(const vector<T>& values) {
+        
+        assert(values.size() > 1);
+        
+        double result = 0;
+        
+        result = sqrt(variance(values) / (values.size() - 1));
+        
+        return result;
+    }
+    
+    template<typename T>
+    double maxValue(const vector<T>& values) {
+        
+        assert(values.size() > 0);
+        
+        double result = *(values.begin());
+        
+        typename vector<T>::const_iterator vectorIterator;
+        
+        for (vectorIterator = values.begin(); vectorIterator != values.end(); ++vectorIterator) {
+            
+            double temp = *vectorIterator;
+            
+            if (result < temp) {
+                result = temp;
+            }
+        }
+        
+        return result;
+    }
+    
+    template<typename T>
+    size_t maxValueIndex(const vector<T>& values) {
+        
+        double maxValue = 0;
+        size_t maxIndex = 0;
+        
+        size_t index = 0;
+        
+        typename vector<T>::const_iterator vectorIterator;
+        
+        for (vectorIterator = values.begin(); vectorIterator != values.end(); ++vectorIterator) {
+            
+            if (vectorIterator == values.begin()) {
+                maxValue = *vectorIterator;
+            } else {
+                if (*vectorIterator > maxValue) {
+                    maxValue = *vectorIterator;
+                    maxIndex = index;
+                }
+            }
+            ++index;
+        }
+        
+        return maxIndex;
+    }
     
     template <typename T>
     inline const map<T, double> distribution(const vector<T>& data) {
